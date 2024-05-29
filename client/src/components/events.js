61f891c791e4d1cefc59554d78d7e255b296c9a2
mandard2015 <mandard1512@gmail.com>
2024-05-29 23:38:57 +0530

    added reviews and events card clickable

--- client/src/components/events.js
+++ client/src/components/events.js
@@ -1,63 +1,101 @@
-import React from 'react';
+import React, { useState } from 'react';
+import Modal from 'react-modal';
 
 const eventsData = [
-  {
-    id: 1,
-    date: '2024-05-11',
-    title: 'Farewell to our guest from Russia ',
-    description: 'A farewell party to our guest Evan.',
-    imageUrl: '/images/Farewell.webp',
-  },{
-    id: 2,
-    date: '2024-04-14',
-    title: 'Maidaan: Movie',
-    description: 'A day out to watch a movie with Aryans Family',
-    imageUrl: '/images/Maidaan2.webp',
-  },
-  {
-    id: 3,
-    date: '2024-03-29',
-    title: 'Sportz Seminar',
-    description: 'Session by: Owners, Technical Staff, Nutritionist, Physio and Founder members',
-    imageUrl: 'path/to/image2.jpg',
-  },
-  {
-    id: 4,
-    date: '2024-03-23',
-    title: 'Ram Vishwakarma Memorial Trophy',
-    description: 'A match held in memory of ....... Ram Vishwakarma.',
-    imageUrl: 'path/to/image3.jpg',
-  },
-  {
-    id: 5,
-    date: '2024-03-29',
-    title: 'Jersey Launch',
-    description: 'Lauch of our new jersey',
-    imageUrl: 'path/to/image2.jpg',
-  },
-  
-  // Add more events as needed
+    {
+        id: 1,
+        date: '2024-05-11',
+        title: 'Farewell to our guest from Russia ',
+        description: 'A farewell party to our guest Evan.',
+        images: ['/images/Farewell.webp', '/images/Farewell.webp'],
+    }, {
+        id: 2,
+        date: '2024-04-14',
+        title: 'Maidaan: Movie',
+        description: 'A day out to watch a movie with Aryans Family',
+        images: ['/images/Maidaan2.webp', '/images/Maidaan1.webp', '/images/Maidaan3.webp'],
+    },
+    {
+        id: 3,
+        date: '2024-03-29',
+        title: 'Sportz Seminar',
+        description: 'Session by: Owners, Technical Staff, Nutritionist, Physio and Founder members',
+        images: ['path/to/image2.jpg', '/images/Farewell.webp'],
+    },
+    {
+        id: 4,
+        date: '2024-03-23',
+        title: 'Ram Vishwakarma Memorial Trophy',
+        description: 'A match held in memory of ....... Ram Vishwakarma.',
+        images: ['path/to/image3.jpg', '/images/Farewell.webp'],
+    },
+    {
+        id: 5,
+        date: '2024-03-29',
+        title: 'Jersey Launch',
+        description: 'Lauch of our new jersey',
+        images: ['path/to/image2.jpg', '/images/Farewell.webp'],
+    },
+
+    // Add more events as needed
 ];
 
 const Events = () => {
-  return (
-    <div className="container mx-auto px-4 py-8 mt-[90px]">
-      <h1 className="gradient-text text-3xl font-bold mb-6 text-center">Our Events</h1>
-      <p className="text-left mx-12 mb-6">At Aryans we organise various Events throughout the Season and encourage each and every entity of the club tto take part in this events</p>
-      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
-        {eventsData.map((event) => (
-          <div key={event.id} className="bg-white shadow-xl rounded-lg p-6">
-            <img src={event.imageUrl} alt={event.title} className="w-full h-48 object-cover rounded-t-lg" />
-            <div className="p-4">
-              <h2 className="text-2xl font-semibold mb-2">{event.title}</h2>
-              <p className="text-gray-600 mb-4">{new Date(event.date).toLocaleDateString()}</p>
-              <p className="text-gray-700">{event.description}</p>
+    const [modalIsOpen, setModalIsOpen] = useState(false);
+    const [selectedEvent, setSelectedEvent] = useState(null);
+
+    const openModal = (event) => {
+        setSelectedEvent(event);
+        setModalIsOpen(true);
+    };
+
+    const closeModal = () => {
+        setSelectedEvent(null);
+        setModalIsOpen(false);
+    };
+
+    return (
+        <div className="container mx-auto px-8 py-8 mt-[80px]">
+            <h1 className="gradient-text text-3xl font-bold mb-4 text-center">Our Events</h1>
+            <p className="text-left text-lg mx-12">At Aryans Football Club, we believe in the power of community and participation. 
+            Throughout the season, we organize a variety of exciting events designed to engage and inspire every member of our club. 
+            From competitive matches and skill-building workshops to community outreach programs and fun-filled family days, there 
+            is something for everyone.</p><p className="text-left text-lg mx-12 mb-4">We encourage all players, coaches, parents, and 
+            supporters to get involved and make the most of these opportunities. Our events not only enhance your football 
+            skills but also foster teamwork, sportsmanship, and a strong sense of belonging. Join us and be a part of the 
+            vibrant Aryans FC community, where every event is a step towards growth and camaraderie.</p>
+            <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
+                {eventsData.map((event) => (
+                    <div key={event.id} className="bg-white shadow-xl rounded-lg p-6 cursor-pointer" onClick={() => openModal(event)}>
+                        <img src={event.images[0]} alt={event.title} className="w-full h-48 object-cover rounded-t-lg" />
+                        <div className="p-4">
+                            <h2 className="text-2xl font-semibold mb-2">{event.title}</h2>
+                            <p className="text-gray-600 mb-4">{new Date(event.date).toLocaleDateString()}</p>
+                            <p className="text-gray-700">{event.description}</p>
+                        </div>
+                    </div>
+                ))}
             </div>
-          </div>
-        ))}
-      </div>
-    </div>
-  );
+
+            {selectedEvent && (
+                <Modal isOpen={modalIsOpen} onRequestClose={closeModal} className="modal" overlayClassName="overlay">
+                    <div className="bg-white p-6 mt-16 rounded-lg">
+                        <h2 className="text-2xl font-bold mb-4">{selectedEvent.title}</h2>
+                        <p className="text-gray-600 mb-4">{new Date(selectedEvent.date).toLocaleDateString()}</p>
+                        <p className="text-gray-700 mb-4">{selectedEvent.description}</p>
+                        <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
+                            {selectedEvent.images.map((image, index) => (
+                                <img key={index} src={image} alt={`${selectedEvent.title} ${index + 1}`} className="w-full h-48 object-cover rounded-lg" />
+                            ))}
+                        </div>
+                        <button onClick={closeModal} className="mt-4 bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+                            Close
+                        </button>
+                    </div>
+                </Modal>
+            )}
+        </div>
+    );
 };
 
 export default Events;
--- client/src/components/home.js
+++ client/src/components/home.js
@@ -1,63 +1,111 @@
 import React from 'react';
+import Slider from 'react-slick';
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
 // import { Link as ScrollLink } from 'react-scroll';
 
+const testimonials = [
+    {
+        id: 1,
+        name: 'John Doe',
+        text: 'Aryans FC has been a fantastic experience for my son. The coaches are professional, and the training facilities are top-notch.',
+        image: 'path/to/image1.jpg',
+    },
+    {
+        id: 2,
+        name: 'Jane Smith',
+        text: 'As a parent, I couldn’t be happier with the progress my child has made at Aryans FC. Highly recommended!',
+        image: 'path/to/image2.jpg',
+    },
+    {
+        id: 3,
+        name: 'Mike Johnson',
+        text: 'The club has a great community spirit, and my daughter loves being a part of it.',
+        image: 'path/to/image3.jpg',
+    },
+];
+
 const Home = () => {
+    const settings = {
+        dots: true,
+        infinite: true,
+        speed: 500,
+
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 3000,
+        pauseOnHover: true,
+    };
+
     return (
         <div>
-            {/* Hero Section */}
             <section className="bg-cover bg-center h-screen mt-16 text-white" style={{ backgroundImage: "url('/images/football1.webp')" }}>
                 <div className="flex items-center justify-center h-full bg-black bg-opacity-50">
-                    <div className="text-center">
+                    <div className="text-center mx-1">
                         <h1 className="text-5xl font-bold mb-4">Welcome to Aryans FC Pune</h1>
                         <p className="text-lg text-left mx-12 mb-6 ">We are delighted to welcome you to our Academy. The Club has a proud tradition of developing
                             home grown footballers, many of whom have gone on to play not only in our first team but have also represented State and
                             Nation.<br />We sincerely hope that you enjoy your experience as a player within our Academy and look forward to seeing you
                             progress through our system</p><p className="text-xl text-right mx-12 mb-6"> ~ Best wishes & Good Luck
-                            Aryans Board Members
+                                Aryans Board Members
                         </p>
                     </div>
                 </div>
             </section>
 
             {/* About Section */}
-            <section className="py-16 bg-gray-100">
-                <div className="container mx-auto px-4">
-                    {/* <h2 className="text-3xl font-bold mb-6 text-center">About Us</h2> */}
-                    <p className="text-gray-700 font-bold text-xl text-left">
-                    Founded in [Year] and nestled in the vibrant city of Pune, Aryans Football Club is a premier institution dedicated 
-                    to nurturing football talent. As a proud member of the Pune District Football Association (PDFA), we are committed 
-                    to maintaining the highest standards of football excellence and community engagement.</p>
-                    <p className="text-gray-700 font-bold text-xl text-left ">At Aryans FC, we believe in the power of youth development. Our 
-                    Academy Performance Plan is crafted to attract, develop, and retain the finest young talents in the PCMC region. 
-                    With top-tier coaching, state-of-the-art facilities, and a supportive environment, we ensure every player has the opportunity to excel and grow.</p>
-                    <p className="text-gray-700 font-bold text-xl text-left ">Join us at Aryans Football Club and become part of a passionate 
-                    football family. We are excited to accompany you on your journey and watch you thrive in our dynamic program. 
-                    Welcome to the next chapter of your football adventure!</p>
-                    {/* AryansFCPune is a passionate football club dedicated to fostering talent and achieving excellence on the field.
+            <section className="py-12 bg-gray-100">
+                <div className="container mx-auto px-4 my-0">
+                    <div className=" flex flex-col md:flex-row my-2 bg-light w-[95%] mx-auto rounded-xl">
+                        <div className="flex-col mt-4">
+                            {/* <h2 className="text-3xl font-bold mb-6 text-center">About Us</h2> */}
+                            <p className="text-gray-700 font-bold text-lg md:text-xl px-6">
+                                Founded in [Year] and nestled in the vibrant city of Pune, Aryans Football Club is a premier institution dedicated
+                                to nurturing football talent. As a proud member of the Pune District Football Association (PDFA), we are committed
+                                to maintaining the highest standards of football excellence and community engagement.</p>
+                            <p className="text-gray-700 font-bold text-lg md:text-xl px-6">At Aryans FC, we believe in the power of youth development. Our
+                                Academy Performance Plan is crafted to attract, develop, and retain the finest young talents in the PCMC region.
+                                With top-tier coaching, state-of-the-art facilities, and a supportive environment, we ensure every player has the opportunity to excel and grow.</p>
+                            <p className="text-gray-700 font-bold text-lg md:text-xl px-6">Join us at Aryans Football Club and become part of a passionate
+                                football family. We are excited to accompany you on your journey and watch you thrive in our dynamic program.
+                                Welcome to the next chapter of your football adventure!</p>
+                            {/* AryansFCPune is a passionate football club dedicated to fostering talent and achieving excellence on the field.
                         Our mission is to provide a platform for aspiring footballers to showcase their skills and reach new heights in their careers. */}
+                        </div>
+                        <div className="md:flex-grow text-center p-4">
+                            <img className="mx-auto w-5/6 rounded-3xl mb-4"
+                                // style={{ position: 'center',
+                                src='/images/Aryans logo.webp' alt='Aryans FC' />
+                            <h4 className='font-bold'>Aryans FC Pune</h4>
+                        </div>
+                    </div>
                 </div>
             </section>
 
             {/* Additional Sections */}
-            <section id="product" className="py-16">
+            <section className="bg-cover bg-center w-[100%] py-16" style={{ backgroundImage: "url('/images/footballpitch.webp')" }}>
                 <div className="container mx-auto px-4">
-                    <h2 className="text-3xl font-bold mb-6 text-center">For Admission and Enquiry Contact</h2>
-                    <h3 className="text-gray-700 text-2xl font-bold text-center">
-                    9657650293
+                    <h2 className="text-3xl text text-stone-900 font-bold mb-4 text-center">For Admission and Enquiry Contact</h2>
+                    <h3 className="text-stone-800 text-2xl font-bold text-center pb-8">
+                        9657650293
                     </h3>
                     {/* Add content about your team here */}
                 </div>
             </section>
 
-            <section id="reviews" className="py-16 bg-gray-100">
-                <div className="container mx-auto px-4">
-                    <h2 className="text-3xl font-bold mb-6 text-center">Reviews</h2>
-                    <p className="text-gray-700 text-center mb-8">
-                        Hear from our fans and players about their experiences with AryansFCPune.
-                    </p>
-                    {/* Add reviews content here */}
-                </div>
-            </section>
+            <div className="container bg-gray-100 rounded-lg min-h-52 w-4/5 my-6 mx-auto px-10">
+                <h2 className="text-2xl py-4 text-center">Reviews</h2>
+                <Slider className='w-[100%]' {...settings}>
+                    {testimonials.map((testimonial) => (
+                        <div key={testimonial.id} className="bg-white shadow-lg rounded-lg p-6">
+                            <p className="text-gray-700 text-center mb-4">{testimonial.text}</p>
+                            <p className="text-gray-900 font-semibold text-center">{testimonial.name}</p>
+                        </div>
+                    ))}
+                </Slider>
+                {/* Add reviews content here */}
+            </div>
         </div>
     );
 };
